c1878b34062f70f7c94a54cffe98b3c3e5e821ff
rukal <rukalad@yandex.ru>
2025-01-27 17:58:21 +0300

    первstart new app

---
+++ apps/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post, Category
+from django_mptt_admin.admin import DjangoMpttAdmin
+# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Post)
+
